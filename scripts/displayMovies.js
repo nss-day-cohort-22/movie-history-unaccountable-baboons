9467b86418573ca9d70e6b7b456508891c4af3c3
Kevin Haggerty <kghaggerty@gmail.com>
2017-11-29 14:58:04 -0600

    movie users table now exists

---
+++ scripts/displayMovies.js
@@ -0,0 +1,5 @@
+const $ = require("jquery")
+
+let firebaseURL = "https://movie-history-2debf.firebaseio.com/movies"
+let firebaseMovieUsersURL = "https://movie-history-2debf.firebaseio.com/movieusers"
+
